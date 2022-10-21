f541de9ca206587853b82d15ec544852bb1a868b
Mai Xuân Phi <101462441+MaiXuanPhi001@users.noreply.github.com>
2022-10-21 13:47:44 +0700

    set empty form login
    set empty form login

--- src/components/screens/ForgotPassword/index.js
+++ src/components/screens/ForgotPassword/index.js
@@ -1,34 +1,19 @@
-// import { StyleSheet, Text, View } from 'react-native'
-// import React from 'react'
-// import Scroll from '../../common/Scroll'
-// import HeaderUser from '../Reuse/HeaderUser'
-// import FormForgot from './FormForgot'
-
-// const ForgotPassword = () => {
-//   return (
-//     <Scroll
-//       flexGrow={1}
-//       alignCenter
-//       paddingBottom={50}
-//     >
-//       <HeaderUser text={'Forgot password'} />
-//       <FormForgot />
-//     </Scroll>
-//   )
-// }
-
-// export default ForgotPassword
-
-// const styles = StyleSheet.create({})
-
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Scroll from '../../common/Scroll'
+import HeaderUser from '../Reuse/HeaderUser'
+import FormForgot from './FormForgot'
 
 const ForgotPassword = () => {
   return (
-    <View>
-      <Text>ForgotPassword</Text>
-    </View>
+    <Scroll
+      flexGrow={1}
+      alignCenter
+      paddingBottom={50}
+    >
+      <HeaderUser text={'Forgot password'} />
+      <FormForgot />
+    </Scroll>
   )
 }
 
